d587539d2fbe706f83f7b1178cc1bde77c07d2dc
TeamBulls <mstteam00@gmail.com>
2026-07-31 19:07:56 -0300

    Update config_v10_7.js

--- config_v10_7.js
+++ config_v10_7.js
@@ -2,5 +2,5 @@
    A chave do App Check/reCAPTCHA Enterprise é pública por definição.
    Não coloque senhas, chaves privadas ou credenciais administrativas aqui. */
 window.TEAM_BULLS_PUBLIC_CONFIG=Object.freeze({
-  appCheckSiteKey: ''
+  appCheckSiteKey: '6Lc3U28tAAAAAB6qyxP8GauRDCg-4ADiy8oYLKXL'
 });
