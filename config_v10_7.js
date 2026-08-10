cde96862bb174ea8e618e6517f500d26d22681d1
TeamBulls <mstteam00@gmail.com>
2026-08-10 17:05:40 -0300

    Load GER bulk controls deterministically

--- config_v10_7.js
+++ config_v10_7.js
@@ -19,7 +19,8 @@ window.TEAM_BULLS_PUBLIC_CONFIG=Object.freeze({
     './modules/cardio-timer-fix-v10_10_9.js?v=10.10.9-cardio1',
     './modules/global-performance-v10_10_9.js?v=10.10.9-perf1',
     './modules/workout-ux-fix-v10_10_9.js?v=10.10.9-workout1',
-    './modules/desktop-performance-v10_10_9.js?v=10.10.9-desktop1'
+    './modules/desktop-performance-v10_10_9.js?v=10.10.9-desktop1',
+    './modules/ger-bulk-v10_10_9.js?v=10.10.9-ger1'
   ];
   const preloadModules=()=>{
     modules.forEach(src=>{
