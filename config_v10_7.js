d063eb85aeb93f2e2315061cefd62d32b7497e00
TeamBulls <mstteam00@gmail.com>
2026-08-11 15:48:02 -0300

    Load direct prescription propagation hotfix

--- config_v10_7.js
+++ config_v10_7.js
@@ -22,6 +22,7 @@ window.TEAM_BULLS_PUBLIC_CONFIG=Object.freeze({
     './modules/desktop-performance-v10_10_9.js?v=10.10.9-desktop1',
     './modules/ger-bulk-v10_10_9.js?v=10.10.9-ger1',
     './modules/prescription-actions-layout-v10_10_9.js?v=10.10.9-actions2',
+    './modules/prescription-propagation-v10_10_9.js?v=10.10.9-propagation1',
     './modules/modal-stack-stability-v10_10_9.js?v=10.10.9-modal2&fix=freeze1'
   ];
   const preloadModules=()=>{
